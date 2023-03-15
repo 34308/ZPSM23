3ee3fa082a786b544f20090aaa51549401fcc208
Broniu <broniekf@gmail.com>
2023-03-15 23:12:17 +0100

    add checking api activity

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
 import SplashScreen from 'react-native-splash-screen';
-import React from 'react';
+import React, { useEffect } from "react";
 
 import loginReducer from './Components/Reducer';
 import {Provider as StoreProvider} from 'react-redux';
 import store from './Components/store';
 import Navigation from './Components/Navigation';
 import FlashMessage from 'react-native-flash-message';
+import { checkIfServerActive } from "./Components/Utilities";
 
 function App() {
   SplashScreen.hide();
+  let checkingInterval = 10000;
+  const checkServerStatus = async () => {
+    if (await checkIfServerActive()) console.log('serwer aktywny');
+    else console.log('serwer nieaktywny');
+  };
+  useEffect(() => {
+    const intervalId = setInterval(checkServerStatus, checkingInterval);
+    return () => clearInterval(intervalId);
+    },[])
   return (
     <StoreProvider store={store}>
       <Navigation />
--- Components/Utilities.js
+++ Components/Utilities.js
@@ -3,6 +3,7 @@ import {LOGOUT} from './actions';
 import store from './store';
 import {showMessage} from 'react-native-flash-message';
 import {COLORS} from './Colors';
+import { re } from "@babel/core/lib/vendor/import-meta-resolve";
 
 export function isTokenExp(token) {
   return jwtDecode(token).exp < Date.now() / 1000;
@@ -49,5 +50,22 @@ export async function checkIfLogged() {
     return false;
   }else{
     return true;
+  }
+}
+
+export async function checkIfServerActive() {
+  const httpTimeout = 3000;
+  const controller = new AbortController();
+  const timeoutId = setTimeout(() => controller.abort(), httpTimeout);
+  try {
+    const response = await fetch('http://10.0.2.2:8082/activity', {signal: controller.signal})
+    console.log(response)
+    if (response.status === 200) {
+      return true
+    } else {
+      return false
+    }
+  } catch (error) {
+    return false
   }
 }
